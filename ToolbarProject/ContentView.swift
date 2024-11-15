2020b3b0094ddad3a6d432e8d75fdb8ce81128aa
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-15 12:02:01 +0000

    Initial commit

---
+++ ToolbarProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToolbarProject
+//
+//  Created by Otavio Lourenço on 15/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Otavio Lourenço on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
